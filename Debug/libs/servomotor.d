83c2264d8fe99aaa261b356537aff57a053929d2
Giovanni Minari <80293487+GioLeiren@users.noreply.github.com>
2023-03-02 23:51:52 -0300

    Add files via upload

---
+++ Debug/libs/servomotor.d
@@ -0,0 +1,21 @@
+# FIXED
+
+libs/servomotor.obj: ../libs/servomotor.c
+libs/servomotor.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h
+libs/servomotor.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430f5529.h
+libs/servomotor.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h
+libs/servomotor.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+libs/servomotor.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../libs/servomotor.c:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430f5529.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
---
+++ Debug/libs/subdir_vars.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../libs/leds.c \
+../libs/sensor.c \
+../libs/servomotor.c 
+
+C_DEPS += \
+./libs/leds.d \
+./libs/sensor.d \
+./libs/servomotor.d 
+
+OBJS += \
+./libs/leds.obj \
+./libs/sensor.obj \
+./libs/servomotor.obj 
+
+OBJS__QUOTED += \
+"libs\leds.obj" \
+"libs\sensor.obj" \
+"libs\servomotor.obj" 
+
+C_DEPS__QUOTED += \
+"libs\leds.d" \
+"libs\sensor.d" \
+"libs\servomotor.d" 
+
+C_SRCS__QUOTED += \
+"../libs/leds.c" \
+"../libs/sensor.c" \
+"../libs/servomotor.c" 
+
+
